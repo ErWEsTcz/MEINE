b8ae44fa634c16e266c35e35f1003804c17c91cf
ErWEsT <nezbedavojta@gmail.com>
2024-10-16 15:59:14 +0200

    Práce se spojovými seznamy

---
+++ Spojovy-seznam/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spojovy_seznam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node uzlik = new Node(8);
+
+        }
+
+        class Node
+        {
+            public Node(int value) // konstruktor třídy Node
+            {
+                Value = value;
+            }
+
+            public int Value { get; }
+
+            public Node Next { get; set; }
+        }
+
+        class LinkedList
+        {
+            public Node Head { get; set; }
+
+            public void Add(int value) // přidat objekt do seznamu
+            {
+                if (Head == null) // když je seznam prázdný
+                {
+                    Head = new Node(value);
+                }
+                else
+                {
+                    Node newNode = new Node(value);
+                    newNode.Next = Head;
+                    Head = newNode;
+                }
+            }
+
+
+            public bool Find(int value)
+            {
+                Node node = Head;
+
+                while (node != null) // dokud nedojedeme na konec seznamu
+                {
+                    if (node.Value == value)
+                    {
+                        return true;
+                    }
+                node = node.Next;
+                }
+                return false;
+            }
+
+            public int FindMinimum()
+            {
+                Node node = Head;
+
+                int Minimum = Head.Next.Value;
+
+                while (node != null) // dokud nedojedeme na konec seznamu
+                {
+                    if (Minimum > node.Value)
+                    {
+                        Minimum = node.Value;
+                    }
+                    node = node.Next;
+                }
+                return Minimum;
+            }
+        }
+    }
+}
