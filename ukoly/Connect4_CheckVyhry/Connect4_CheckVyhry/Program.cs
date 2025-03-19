238dfaa44021f315b3354d10c72ed6877b163bc2
ErWEsT <nezbedavojta@gmail.com>
2025-03-19 15:59:37 +0100

    Connect4-CheckVyhry

---
+++ ukoly/Connect4_CheckVyhry/Connect4_CheckVyhry/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4_CheckVyhry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void CheckVyhry(int[,] hraciPole, int PocetNaVyhru, int[] PosledniZeton)
+        {
+
+            bool CheckRow()
+            {
+                for (int i = PosledniZeton[0] - PocetNaVyhru + 1; i < PocetNaVyhru + PosledniZeton[0] - 1; i++)
+                {
+                    
+                }
+
+                return true;
+            }
+
+
+            return;
+        }
+    }
+}
