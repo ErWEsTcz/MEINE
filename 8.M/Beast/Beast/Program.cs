50419054238a59859c4ff2d67961e56996bf3790
ErWEsTcz <nezbedavojta@gmail.com>
2025-09-15 09:41:09 +0200

    Beast

---
+++ 8.M/Beast/Beast/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BeastInLabyrint kekw = new BeastInLabyrint(10, 6);
+
+            kekw.Print();
+
+            kekw.NajdiPriseru();
+
+            Console.ReadLine();
+        }
+
+    }
+
+   
+    class BeastInLabyrint
+    {
+        public BeastInLabyrint(int sirka, int vyska)
+        {
+            Sirka = sirka;
+            Vyska = vyska;
+            Labyrint = new char[sirka,vyska];
+            PozicePrisery = new int[2];
+            PrecteniLabyrintu();
+        }
+        int Sirka { get; }
+        int Vyska { get; }
+        char[,] Labyrint { get; set; }
+        int[] PozicePrisery { get; set; }
+        char SmerPrisery { get; set; }
+
+        public void NajdiPriseru()
+        {
+            for (int i = 0; i < Vyska; i++)
+            {
+                for (int j = 0; j < Sirka; j++)
+                {
+                    if (Labyrint[i,j] != 'X' & Labyrint[j,i] != '.')
+                    {
+                        PozicePrisery[0] = j;
+                        PozicePrisery[1] = i;
+                        SmerPrisery = Labyrint[i,j];
+                        Console.WriteLine(PozicePrisery[0]);
+                        Console.WriteLine(PozicePrisery[1]);
+                        Console.WriteLine(SmerPrisery);
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void PrecteniLabyrintu()
+        {
+            for (int i = 0; i < Vyska; i++)
+            {
+                string radek = Console.ReadLine();
+
+                for (int j = 0; j < Sirka; j++)
+                {
+                    Labyrint[j, i] = radek[j];
+                }
+            }
+            return;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Vyska; i++)
+            {
+                StringBuilder radek = new StringBuilder();
+                for (int j = 0; j < Sirka; j++)
+                {
+                    radek.Append(Labyrint[j, i]);
+                }
+                Console.WriteLine(radek.ToString());
+            }
+        }
+    }
+}
+
---
+++ 8.M/Beast/Beast/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Beast")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GCHD")]
+[assembly: AssemblyProduct("Beast")]
+[assembly: AssemblyCopyright("Copyright © GCHD 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("bd2c0946-d84e-4afb-bb00-9c960a8eab09")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
