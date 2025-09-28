e51b519570a34ec448dc5318cdc007d8d086fa73
ErWEsT <nezbedavojta@gmail.com>
2025-09-29 00:40:04 +0200

    Beast - hoto

--- 8.M/Beast/Beast/Program.cs
+++ 8.M/Beast/Beast/Program.cs
@@ -10,11 +10,16 @@ namespace Beast
     {
         static void Main(string[] args)
         {
-            BeastInLabyrint kekw = new BeastInLabyrint(10, 6);
+            Console.WriteLine("zadejte šírku pole");
+            int sirka = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("zadejte výšku pole");
+            int vyska = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
 
-            kekw.Print();
+            BeastInLabyrint kekw = new BeastInLabyrint(sirka, vyska);
 
-            kekw.NajdiPriseru();
+            kekw.ProjetiHrou(20);
 
             Console.ReadLine();
         }
@@ -28,31 +33,38 @@ namespace Beast
         {
             Sirka = sirka;
             Vyska = vyska;
-            Labyrint = new char[sirka,vyska];
+            Labyrint = new char[vyska, sirka];
             PozicePrisery = new int[2];
+            ZnamenkaPrisery = new char[4] { '>' , 'v', '<', '^' };
+            SmeryPrisery = new int[4, 2] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
             PrecteniLabyrintu();
         }
         int Sirka { get; }
         int Vyska { get; }
         char[,] Labyrint { get; set; }
         int[] PozicePrisery { get; set; }
-        char SmerPrisery { get; set; }
-
+        char[] ZnamenkaPrisery { get; set; }
+        int[,] SmeryPrisery {  get; set; }
+        int AktualniSmer {  get; set; }
         public void NajdiPriseru()
         {
             for (int i = 0; i < Vyska; i++)
             {
                 for (int j = 0; j < Sirka; j++)
                 {
-                    if (Labyrint[i,j] != 'X' & Labyrint[j,i] != '.')
+                    if (Labyrint[i,j] != 'X' & Labyrint[i,j] != '.')
                     {
-                        PozicePrisery[0] = j;
-                        PozicePrisery[1] = i;
-                        SmerPrisery = Labyrint[i,j];
-                        Console.WriteLine(PozicePrisery[0]);
-                        Console.WriteLine(PozicePrisery[1]);
-                        Console.WriteLine(SmerPrisery);
-                        return;
+                        PozicePrisery[0] = i;
+                        PozicePrisery[1] = j;
+                        char Znamenko = Labyrint[i, j];
+                        for (int z = 0; z < 4; z++)
+                        {
+                            if(Znamenko == ZnamenkaPrisery[z])
+                            {
+                                AktualniSmer = z;
+                                break;
+                            }
+                        }
                     }
                 }
             }
@@ -60,13 +72,14 @@ namespace Beast
 
         public void PrecteniLabyrintu()
         {
+            Console.WriteLine("vložte pole");
             for (int i = 0; i < Vyska; i++)
             {
                 string radek = Console.ReadLine();
 
                 for (int j = 0; j < Sirka; j++)
                 {
-                    Labyrint[j, i] = radek[j];
+                    Labyrint[i, j] = radek[j];
                 }
             }
             return;
@@ -79,9 +92,50 @@ namespace Beast
                 StringBuilder radek = new StringBuilder();
                 for (int j = 0; j < Sirka; j++)
                 {
-                    radek.Append(Labyrint[j, i]);
+                    radek.Append(Labyrint[i, j]);
                 }
                 Console.WriteLine(radek.ToString());
+            }
+        }
+
+        public void ProjetiHrou(int pocetTahu)
+        {
+            NajdiPriseru();
+            for (int i = 1; i < pocetTahu+1; i++)
+            {
+                if (Labyrint[PozicePrisery[0] + SmeryPrisery[AktualniSmer, 1], PozicePrisery[1] + (-1 * SmeryPrisery[AktualniSmer, 0])] == '.')
+                {
+                    AktualniSmer = (AktualniSmer + 1) % 4;
+                    Labyrint[PozicePrisery[0], PozicePrisery[1]] = ZnamenkaPrisery[AktualniSmer];
+                    Console.Clear();
+                    Console.WriteLine(i + ". krok");
+                    Print();
+                    Console.ReadLine();
+                    i++;
+                    if (i > pocetTahu)
+                        break;
+                    Labyrint[PozicePrisery[0], PozicePrisery[1]] = '.';
+                    PozicePrisery[0] = PozicePrisery[0] + SmeryPrisery[AktualniSmer, 0];
+                    PozicePrisery[1] = PozicePrisery[1] + SmeryPrisery[AktualniSmer, 1];
+                    Labyrint[PozicePrisery[0], PozicePrisery[1]] = ZnamenkaPrisery[AktualniSmer];
+
+                }
+                else if (Labyrint[PozicePrisery[0] + SmeryPrisery[AktualniSmer, 0], PozicePrisery[1] + SmeryPrisery[AktualniSmer, 1]] == 'X')
+                {
+                    AktualniSmer = (AktualniSmer + 3) % 4;
+                    Labyrint[PozicePrisery[0], PozicePrisery[1]] = ZnamenkaPrisery[AktualniSmer];
+                }
+                else
+                {
+                    Labyrint[PozicePrisery[0], PozicePrisery[1]] = '.';
+                    PozicePrisery[0] = PozicePrisery[0] + SmeryPrisery[AktualniSmer, 0];
+                    PozicePrisery[1] = PozicePrisery[1] + SmeryPrisery[AktualniSmer, 1];
+                    Labyrint[PozicePrisery[0], PozicePrisery[1]] = ZnamenkaPrisery[AktualniSmer];
+                }
+                Console.Clear();
+                Console.WriteLine(i + ". krok");
+                Print();
+                Console.ReadLine();
             }
         }
     }
