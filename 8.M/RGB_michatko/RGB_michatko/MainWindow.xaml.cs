24ead15bb4079f69ca0efabb9e7ee233e71b0ed1
ErWEsTskola <nezbedavojta@gmail.com>
2026-01-05 09:43:04 +0100

    WPF

---
+++ 8.M/RGB_michatko/RGB_michatko/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RGB_michatko
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ModryText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(ModryText.Text != string.Empty)
+            {
+                try
+                {
+                    ModrePosouvatko.Value = Convert.ToDouble(ModryText.Text);
+                    ZmenaBarvy();
+                }
+                catch
+                {
+                    MessageBox.Show("Kriple");
+                }
+            }
+        }
+
+        private void ModrePosouvatko_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ModryText.Text = Convert.ToString(ModrePosouvatko.Value);
+        }
+
+        public void ZmenaBarvy()
+        {
+            byte r = Convert.ToByte(CervenePosouvatko.Value);
+            byte g = Convert.ToByte(ZelenePosouvatko.Value);
+            byte b = Convert.ToByte(ModrePosouvatko.Value);
+
+            Obdelnik.Fill = new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+    }
+}
---
+++ 8.M/WPF_uvod/WPF_uvod/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_uvod
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Klik(object sender, RoutedEventArgs e)
+        {
+            if (txtPozdrav.Text == "")
+                MessageBox.Show("vstupni pole je prazdne");
+            else
+                btmPozdrav.Content = txtPozdrav.Text;
+        }
+
+        private void btmPozdrav_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //r,g,b
+            Random rnd = new Random();
+            byte r = (byte)rnd.Next(255);
+            byte g = (byte)rnd.Next(255);
+            byte b = (byte)rnd.Next(255);
+
+            //barva tlacitka
+            btmPozdrav.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+    }
+}
