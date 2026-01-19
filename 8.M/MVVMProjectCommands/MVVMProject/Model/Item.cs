d66f8c5553b5dcd368433bdb9660b5091c2ccdcf
ErWEsTskola <nezbedavojta@gmail.com>
2026-01-19 09:37:24 +0100

    TODO list

---
+++ 8.M/MVVMProjectCommands/MVVMProject/Model/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMProject.Model
+{
+    internal class Item
+    {
+        public string Name { get; set; }    
+
+        public DateOnly DeadLine { get; set; }
+    }
+}
